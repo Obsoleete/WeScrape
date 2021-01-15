794276691657f2b8d5c288b6154dbe46664e3da3
Komal Kaler <kkomal2012@hotmail.com>
2021-01-15 17:57:26 -0500

    Added a success screen

--- actions.py
+++ actions.py
@@ -3,51 +3,58 @@ import requests
 import json
 
 # Gets the html content for the given url
-def get_content(url):
+def get_content(urls):
 
-    response = requests.get(url, timeout=5)
-    content = BeautifulSoup(response.content, "html.parser")
+    responses = [requests.get(url, timeout=5) for url in urls]
+    contents = [BeautifulSoup(response.content, "html.parser") for response in responses]
 
-    return content
+    return contents
 
 # Converts html content to json
-def convert_to_json(content):
+def convert_to_json(contents):
 
-    json_array = []
+    json_arrays = []
 
     # Finds the needed data on the website
-    historical_data = content.find('table', attrs={"data-test": "historical-prices"})
-    table_body = historical_data.find('tbody')
+    historical_datas = [content.find('table', attrs={"data-test": "historical-prices"}) for content in contents]
+    table_bodies = [historical_data.find('tbody') for historical_data in historical_datas]
 
     # Creates the array of json objects
-    for row in table_body.findAll('tr'):
+    for table_body in table_bodies:
 
-        data = row.findAll('td')
+        json_array = []
 
-        json_object = {
-            "Date": data[0].get_text(),
-            "Open": data[1].get_text(),
-            "High": data[2].get_text(),
-            "Low": data[3].get_text(),
-            "Close": data[4].get_text(), 
-            "Adj Close": data[5].get_text(),
-            "Volume": data[6].get_text()
-        }
+        for row in table_body.findAll('tr'):
 
-        json_array.append(json_object)
+            data = row.findAll('td')
 
-    return json_array
+            json_object = {
+                "Date": data[0].get_text(),
+                "Open": data[1].get_text(),
+                "High": data[2].get_text(),
+                "Low": data[3].get_text(),
+                "Close": data[4].get_text(), 
+                "Adj Close": data[5].get_text(),
+                "Volume": data[6].get_text()
+            }
+
+            json_array.append(json_object)
+        
+        json_arrays.append(json_array)
+
+    return json_arrays
 
 # Exports a given array of json objects to a json file
-def export_to_json(json_array):
+def export_to_json(json_arrays):
 
-    with open('stockData.json', 'w') as outfile:
-        json.dump(json_array, outfile)
+    for i in range(1, len(json_arrays) + 1):
+        with open('stockData' + str(i) + '.json', 'w') as outfile:
+            json.dump(json_arrays[i-1], outfile)
 
 
 if __name__ == "__main__":
 
     # Test functions
-    content = get_content("https://ca.finance.yahoo.com/quote/CL%3DF/history?p=CL%3DF")
+    content = get_content(["https://ca.finance.yahoo.com/quote/CL%3DF/history?p=CL%3DF"])
     json_array = convert_to_json(content)
     export_to_json(json_array)
--- main.py
+++ main.py
@@ -6,32 +6,56 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen
 from actions import get_content, convert_to_json, export_to_json
-
-#from kivy.network.urlrequest import UrlRequest
-#import requests
-
-#Builder.load_file('WeScrape.kv')
 
 #sets background colour
 Window.clearcolor = (97/255, 60/255, 102/255, 1)
-
+inputs=[]
 class WeScrapeApp(App):
     def build(self):
+        self.sm = ScreenManager()
+        self.screen1 = Screen(name='first')
+        self.screen2 = Screen(name='second')
+        self.sm.add_widget(self.screen1)
+        self.sm.add_widget(self.screen2)
         self.layout = BoxLayout(orientation='vertical',padding=100,spacing=15)
         self.logo = Label(text='WeScrape',font_size=40)
-        self.html = TextInput(text='Enter site url here!',size_hint=(1,0.1),multiline=False)
-        submit = Button(background_color = [0, 240, 123, 0.5], text='Extract to JSON File', pos_hint={'center_x':0.5},size_hint=(None,None), width=150,height=50, on_press=self.submit) 
+        self.html = TextInput(text='Enter site url here!',size_hint=(0.9,None),height=30,multiline=False)
+        inputs.append(self.html)
+        self.insideLayout = BoxLayout(orientation='horizontal')
+        self.submitbutton = Button(background_color = [0, 240, 123, 0.5], text='Extract to JSON File', pos_hint={'center_x':0.5},size_hint=(None,None), width=150,height=50, on_press=self.submit) 
+        addbutton = Button(background_color = [0, 240, 123, 1], text='+', pos_hint={'center_x':0.5},size_hint=(None,None), width=30,height=30, on_press=self.addlink) 
         #adds widget to screen
         self.layout.add_widget(self.logo)
-        self.layout.add_widget(self.html)
-        self.layout.add_widget(submit)
-        return self.layout   
+        self.insideLayout.add_widget(self.html)
+        self.insideLayout.add_widget(addbutton)
+        self.layout.add_widget(self.insideLayout)
+        self.layout.add_widget(self.submitbutton)
+        self.screen1.add_widget(self.layout)
+        self.returnButton = Button(background_color = [0, 240, 123, 0.5], text='Return', pos_hint={'center_x':0.5},size_hint=(None,None), width=150,height=50, on_press=self.retToHome) 
+        box = BoxLayout(orientation='vertical',padding=100,spacing=15)
+        box.add_widget(self.returnButton)
+        self.screen2.add_widget(box)
+        return self.sm   
     
     # Gets html content and extracts the needed information which is stored in a JSON file
-    def submit(self,obj):
+    def addlink(self,obj):
+        new_html = TextInput(text='Enter another site url here!',size_hint=(0.95,None),height=30,multiline=False)
+        inputs.append(new_html)
+        self.layout.remove_widget(self.submitbutton)
+        self.layout.add_widget(new_html)
+        self.layout.add_widget(self.submitbutton)
 
-        content = get_content(self.html.text)
+    def retToHome(self,obj):
+        self.sm.switch_to(self.screen1, direction='right')
+
+    def submit(self,obj):
+        urls = []
+        for widget in inputs:
+            urls.append(widget.text)
+        self.sm.switch_to(self.screen2, direction='left')
+        content = get_content(urls)
         json_array = convert_to_json(content)
         export_to_json(json_array)
 
